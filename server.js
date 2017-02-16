e15a1174e7177d42067037f36c7c7d92da86a02f
Vinay Reddy <vinayreddy91.in@gmail.com>
2017-02-16 17:58:40 -0600

    node initial practice commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+// app.get('/', function(req, res){
+//     res.send('Hello Express!');
+// });
+
+var middleware = require('./middleware');
+
+app.use(middleware.requireAuthentication);
+
+app.use(middleware.logger);
+
+app.get('/about', function(req, res){
+	res.send('Welcome to Node World. This is the Node Web module!!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('Express server started on port ' + PORT +'!');
+});
