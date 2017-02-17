ec80891aeab028443d20798059eb0e2a1149ae72
Vinay Reddy <vinayreddy91.in@gmail.com>
2017-02-17 17:33:41 -0600

    updated port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // app.get('/', function(req, res){
 //     res.send('Hello Express!');
